39a8ac9f0d602f67dc7cafac627891c092994342
ravikantpal <ravikantpal123@gmail.com>
2020-08-19 23:09:18 +0530

    added react router and did basic routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './App.css';
 import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 function App() {
   return (
-    <div className="App">
-        <Dashboard/>
-    </div>
+      <Router>
+        <div className="App">
+            <Header />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
   );
 }
 
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
+import ProjectItems from "./Project/ProjectItems";
+import CreateReactButton from "./Project/CreateProjectButton";
+
 
 class Dashboard extends Component {
     render() {
         return (
-            <div>
-               <h1>Welcome to Dashboard</h1>
+
+            <div className="projects">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-4 text-center">Projects</h1>
+                            <br/>
+                            <CreateReactButton/>
+                            <br/>
+                            <hr/>
+
+                            <ProjectItems/>
+
+
+                        </div>
+                    </div>
+                </div>
             </div>
+
         );
     }
 }
