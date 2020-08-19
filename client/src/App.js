a655dfb6225ac026d4e85e8ef3fcf2bd98d4feae
ravikantpal <ravikantpal123@gmail.com>
2020-08-19 17:32:28 +0530

    created dashboard component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Holla up a minute
-      </header>
+        <Dashboard/>
     </div>
   );
 }
