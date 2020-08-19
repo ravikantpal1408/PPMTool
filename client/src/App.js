420f6107e48d95151bbb5dd00401b7293a19d087
ravikantpal <ravikantpal123@gmail.com>
2020-08-19 17:29:03 +0530

    created react client application

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Holla up a minute
+      </header>
+    </div>
+  );
+}
+
+export default App;
