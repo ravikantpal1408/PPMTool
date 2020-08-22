490e54dade8cfa0752baa511046c4441638e270d
ravikantpal <ravikantpal123@gmail.com>
2020-08-22 14:35:25 +0530

    did some code refactoring

--- client/src/actions/projectActions.js
+++ client/src/actions/projectActions.js
@@ -1,15 +1,28 @@
 import axios from 'axios';
 import {GET_ERRORS} from "./types";
 
-export const createProject = (project, history)  => async dispatch => {
-    try{
-        const res = axios.post("http://localhost:8080/api/project", project);
-        history.push("/dashboard");
-    }
-    catch (e) {
+
+const baseURL = "http://localhost:8080/api";
+
+
+export const createProject = (project, history) => async dispatch => {
+    try {
+        axios.post(baseURL + "/project", project)
+            .then(res => {
+                // console.log(res)
+                history.push("/dashboard");
+            }).catch(error => {
+            // console.log("fata ", error)
+            // console.log(error.response.data)
+            dispatch({
+                type: GET_ERRORS,
+                payload: error.response.data
+            })
+        });
+    } catch (e) {
         dispatch({
             type: GET_ERRORS,
-            payload : e.response.data
+            payload: e.response.data
         })
     }
 }
