87bbbfeb6521243e6288225c643224903643a6e5
ravipal <ravikantpal123@gmail.com>
2020-09-26 08:42:19 +0530

    added MS ACCESS DB MAVEN PACKAGE

--- client/src/components/Project/ProjectItems.js
+++ client/src/components/Project/ProjectItems.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 
 class ProjectItems extends Component {
     render() {
-        const { project } = this.props;
+        const {project} = this.props;
         return (
             <div className="container">
                 <div className="card card-body bg-light mb-3">
@@ -17,21 +17,21 @@ class ProjectItems extends Component {
                         </div>
                         <div className="col-md-4 d-none d-lg-block">
                             <ul className="list-group">
-                                <a href="#">
+                                <Link>
                                     <li className="list-group-item board">
                                         <i className="fa fa-flag-checkered pr-1"> Project Board </i>
                                     </li>
-                                </a>
+                                </Link>
                                 <Link to={'updateProject/' + project.projectIdentifier}>
                                     <li className="list-group-item update">
                                         <i className="fa fa-edit pr-1"> Update Project Info</i>
                                     </li>
                                 </Link>
-                                <a href="#">
+                                <Link>
                                     <li className="list-group-item delete">
                                         <i className="fa fa-minus-circle pr-1"> Delete Project</i>
                                     </li>
-                                </a>
+                                </Link>
                             </ul>
                         </div>
                     </div>
